30347c6aa923ae6d7fb60720b7087c74fe2dcce2
jonpfowler <jonfo@outlook.com>
2015-07-09 14:53:22 -0700

    updates

--- VuGen/Scripts/OpticTest/vuser_init.c
+++ VuGen/Scripts/OpticTest/vuser_init.c
@@ -1,20 +1,21 @@
 vuser_init()
 {
-	int retval = 0;
+	int retValue;
 	long counterValue;
 
-	retval = lr_load_dll("C:\\Users\\conjpf\\Sources\\Codeplex\\Optic2\\Release\\Optic.dll");
+	retValue = lr_load_dll("C:\\Users\\conjpf\\Sources\\Codeplex\\Optic2\\Release\\Optic.dll");
+   	lr_log_message("Return value from lr_load_dll(): %d", retValue);
 	
-    IncrementCounter("LoadRunner(VUsers)\\Count", 1);
-
-    counterValue = GetCounter("LoadRunner(VUsers)\\Count");
-   	lr_log_message("LoadRunner(VUsers)\\Count: %d", counterValue);
-
-	counterValue = GetCounter("Processor Information(_Total)\\% Processor Time");
+	//This only requires read access to registry
+   	counterValue = GetCounter("Processor Information(_Total)\\% Processor Time");
    	lr_log_message("Processor Information(_Total)\\% Processor Time: %d", counterValue);
 
-//   	counterValue = GetCounter("Process(services)\\Handle Count");
-//   	lr_log_message("counter: %d", counterValue);
+    //Must have registry write privileges to create a counter
+	IncrementCounter("LoadRunner(VUsers)\\Count", 1);
+
+    //Counter must exist or an exception will be raised
+	counterValue = GetCounter("LoadRunner(VUsers)\\Count");
+   	lr_log_message("LoadRunner(VUsers)\\Count: %d", counterValue);
 
 	return 0;
 }
